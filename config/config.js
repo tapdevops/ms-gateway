967809ce64f3318575142496695693ecfb14f159
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2018-11-07 16:47:03 +0700

    Ferdinand

--- config/config.js
+++ config/config.js
@@ -1,12 +1,13 @@
 module.exports = {
+	//
 	app_port: process.env.PORT || 3001,
 	app_name: 'Microservices Gateway',
 	secret_key: 'T4pagri123#',
 	url: {
 		microservices: {
 			inspection: 			'http://149.129.242.205:3002/api/inspection',
-			masterdata_block: 		'http://149.129.242.205:3004/block',
-			masterdata_afdeling: 	'http://149.129.242.205:3004/afdeling',
+			masterdata_block: 		'http://149.129.242.205:3002/block',
+			masterdata_afdeling: 	'http://149.129.242.205:3002/afdeling',
 			ldap: 					'http://tap-ldapdev.tap-agri.com/login'
 		}
 	}
