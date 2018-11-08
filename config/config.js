db20e776e11c0dee9ceb7ecb02f3178b5b3fe6b2
root <whenawijanarko08@gmail.com>
2018-11-08 10:47:23 +0800

    Ferdinand

--- config/config.js
+++ config/config.js
@@ -7,8 +7,8 @@ module.exports = {
 		microservices: {
 			inspection: 			'http://149.129.242.205:3002/api/inspection',
 			masterdata_block: 		'http://149.129.242.205:3002/block',
-			masterdata_afdeling: 	'http://149.129.242.205:3002/afdeling',
-			ldap: 					'http://tap-ldapdev.tap-agri.com/login'
+			masterdata_afdeling: 		'http://149.129.242.205:3002/afdeling',
+			ldap: 				'http://tap-ldapdev.tap-agri.com/login'
 		}
 	}
 }
