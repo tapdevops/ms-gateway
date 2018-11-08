7c1874bca7856bd2754d612c6269edceac163253
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2018-11-08 09:51:21 +0700

    Ferdinand

--- config/database.js
+++ config/database.js
@@ -1,7 +1,3 @@
 module.exports = {
 	url: 'mongodb://user-db:tap123456me@clustertest-shard-00-00-iwwfu.gcp.mongodb.net:27017/ms_gateway?authSource=admin'
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> fdbc55c6a32e8d28268003d000e154419e422c47
