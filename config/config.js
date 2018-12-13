f17f0e50e40df7b9cc7f45c253c631a386d731b5
tapdevops <44764573+tapdevops@users.noreply.github.com>
2018-12-13 16:13:06 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
 	| App Config
 	|--------------------------------------------------------------------------
 	*/
-	app_port: process.env.PORT || 3006,
+	app_port: process.env.PORT || 3002,
 	app_name: 'Microservice Auth',
 
 	/*
